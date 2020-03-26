8581543077fd764b490e17f5e3c1fe1d4849f06b
richknicks <rlynch@gmail.com>
2020-03-26 16:41:45 -0400

    removed comments

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -8,21 +8,7 @@ it("should run the tests", function() {
 describe('enhancer.js', () => {
     describe('.repair()', () => {
         it('returns item durability to 100', () => {
-            // // Arrange
-            // const sword = {
-            //     name: 'Sword of 1k Truths',
-            //     durability: 10,
-            //     enchancement: 20
-            // };
-            // const expected = {
-            //     name: sword.name,
-            //     durability: 100,
-            //     enchancement: sword.enchancement
-            // }
-            // // Act
-            // const repairedItem = repair(sword);
-            // // Assert
-            // expect(repairedItem).toEqual(expected);
+          
             expect(repair({ durability: 10 }).durability).toBe(100);
             expect(repair({ durability: 19 }).durability).toBe(100);
             expect(repair({ durability: 100 }).durability).toBe(100);
@@ -30,21 +16,7 @@ describe('enhancer.js', () => {
     });
     describe('.succeed()', () => {
         it('should return an item with an increased enhancement level of + 1', () => {
-        //     // Arrange
-        //    const hammer = {
-        //         name: 'Mjölnir',
-        //         durability: 100,
-        //         enchancement: 10
-        //     };
-        //     const expected = {
-        //         name: hammer.name,
-        //         durability: hammer.durability,
-        //         enchancement: hammer.enchancement+1
-        //     }
-        //     // Act
-        //     const enhancedItem = succeed(hammer);
-        //     // Assert
-        //     expect(enhancedItem).toEqual(expected);
+   
               expect(succeed({ enhancement: 10 }).enhancement).toBe(11);
               expect(succeed({ enhancement: 19 }).enhancement).toBe(20);
               expect(succeed({ enhancement: 20 }).enhancement).toBe(20);
